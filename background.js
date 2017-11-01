3c6f53f0a8c33f51b6d4596b58c6ad3cf6b29f3c
Alex Ward <alexander.ward@skybettingandgaming.com>
2017-11-01 09:38:49 +0000

    Oops

--- background.js
+++ background.js
@@ -12,18 +12,22 @@ chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
   var currentUrl = tab.url;
 
   chrome.storage.sync.get('items', function(data) {
-    for (var i = 0; i < data.items.length; i++) {
-      var item = data.items[i];
+    if (data.items) {
+      for (var i = 0; i < data.items.length; i++) {
+        var item = data.items[i];
+  
+        if (currentUrl.match(item.regex)) {
+          ctx.fillStyle = item.colour;
+  
+          clearCanvas();
+          drawCircle();
+        
+          var icon = canvas.toDataURL();
+        
+          chrome.tabs.sendMessage(tabId, { icon: icon });
 
-      if (currentUrl.match(item.regex)) {
-        ctx.fillStyle = item.colour;
-
-        clearCanvas();
-        drawCircle();
-      
-        var icon = canvas.toDataURL();
-      
-        chrome.tabs.sendMessage(tabId, { icon: icon });
+          break;
+        }
       }
     }
   });
